b28815607bf2795e424c1441463f9a937bfd75af
Elias Paulino <eliaspaulinom@gmail.com>
2013-04-23 17:35:21 -0300

    refactoring get skill

---
+++ test/us01/SkillInTimeMethodTest.java
@@ -0,0 +1,14 @@
+package us01;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SkillInTimeMethodTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
