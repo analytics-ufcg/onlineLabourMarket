6dbb209937f70c4a11ed2d5fc6c72916e8dba108
alberto fagner ferreira de barros <albertofagner.cav@gmail.com>
2013-04-24 17:26:40 -0300

    testing and refactoring  python code

--- src/us01/getSkillRanking.py
+++ src/us01/getSkillRanking.py
@@ -1,77 +1,43 @@
 # import to use command-line arguments
+
 import sys
 
-def processaArquivo(arquivoEntrada,arquivoSaida,lista,separador):
-    arqIn = open(arquivoEntrada,"r")
-    arqOut = open(arquivoSaida,"w")
-    for colNome in lista:
-        arqOut.write(colNome)
-        if(colNome!=lista[-1]):
-            arqOut.write(",")
-        else:
-            arqOut.write("\n")
-    for linha in arqIn.readlines():
-        listaDados = linha.split(separador)
-        for dado in listaDados:
-            arqOut.write(dado)
-            if(dado!=listaDados[-1]):
-                arqOut.write(",")
-    arqIn.close()
-    arqOut.close()
-
-def extrairAtributos(arquivoEn,arquivoSa,listaPos,sep):
-    arqEn = open(arquivoEn,"r")
-    arqSa = open(arquivoSa,"w")
-    for linha in arqEn.readlines():
-        listaDados = linha.split(sep)
-        for pos in listaPos:
-            arqSa.write(listaDados[pos])
-            if(pos!=listaPos[-1]):
-                arqSa.write(",")
-        arqSa.write("\n")
-    arqEn.close()
-    arqSa.close()
-
-def contFrequencia(arquivoIn,arquivoOut,separador):
-    lista = []
-    skills = []
-    freq = []
-    arq = open(arquivoIn,"r")
-    for linha in arq.readlines():
-        temp = linha.split(",")
-        if(temp[0] in lista):
+# Produce a ranking of skills based in how many different tasks it appears on
+def contFrequencia(inputFileName,idIndex,skillIndex,sep="&|&",skillSep=";"):
+    print inputFileName
+    processedIDs = []
+    ranking = {}
+    inputFile = open(str(inputFileName), "r")
+    for line in inputFile.readlines():
+        currentLine = line.split(sep)
+        currentID = currentLine[idIndex]
+        if(currentID in processedIDs):
             continue;
         else:
-            lista.append(temp[0])
-            sks = temp[1].split(separador)
-            for sk in sks:
-                sk = sk.replace("\n","")
-                if(sk in skills):
-                    val = skills.index(sk)
-                    freq[val] = freq[val]+1
+            processedIDs.append(currentID)
+            currentSkills = currentLine[skillIndex].rstrip().split(skillSep)
+            for skill in currentSkills:
+                if(skill in ranking):
+                    ranking[skill] += 1
                 else:
-                    skills.append(sk)
-                    freq.append(1)
-    arq.close()
-    lista=[]
-    for i in range(len(skills)):
-        lista.append(skills[i]+";"+str(freq[i])+"\n")
-    lista = sorted(lista, cmp=compare)
-    arqS = open(arquivoOut,"w")
-    for k in lista:
-        arqS.write(k)
-    arqS.close()
- 
-def compare(x, y):
-    temp = x.split(";")
-    temp2 = y.split(";")
-    num = int(temp[1])
-    num2 = int(temp2[1])
-    return num2-num
+                    ranking[skill] = 1
+    inputFile.close()
+    return ranking
+
 
 def main():
-    extrairAtributos(str(sys.argv[1]),str(sys.argv[2]), [2,12],"&|&")
-    contFrequencia(str(sys.argv[2]),str(sys.argv[2]),";")
+    inputFileName = str(sys.argv[1])
+    outputFileName = str(sys.argv[2])
+    idIndex = sys.argv[3]
+    skillIndex = sys.argv[4]
+    
+#    rankingCode = GetSkillRanking()
+    
+    ranking = contFrequencia(inputFileName, idIndex, skillIndex)
+    outputFile = open(outputFileName,"w")
+    for w in sorted(ranking, key=ranking.get, reverse=True):
+        outputFile.write(w +";" + str(ranking[w])+"\n")
+    outputFile.close()
 
 
 if  __name__ =='__main__':main()
