e7bd73bbcd834f2be317c6ed7e9b54bbba765bb0
Giovani Cavalcante Barbosa <giovanibarbosa@gmail.com>
2013-05-13 19:40:56 -0300

    Script para gerar arquivos de habilidades por dia para retribuição

---
+++ src/main/R/buildRetributionFiles.r
@@ -0,0 +1,42 @@
+date.from.timestamp =  function(ts){
+  return(strptime(structure(ts, class=c("POSIXt", "POSIXct")), "%Y-%m-%d"))
+}
+
+args = commandArgs(TRUE)
+source.dir = as.character(args[1])
+
+files = list.files(source.dir, full.names=T, pattern=".csv")
+
+output.dir = paste(source.dir, "byDay", sep="/")
+dir.create(output.dir, showWarnings=F)
+
+generating.csv.file = function(f) {
+  dados = read.csv(f)
+  dados$V4 = date.from.timestamp(dados$currentTime)
+  dados$lista = gsub(" ", "", as.character(dados$lista))
+  ag = with(dados, aggregate(lista, list(as.numeric(V4)), paste))
+  rm(dados)
+  for(i in c(1:nrow(ag))) {
+    numeric.vector = as.numeric(unlist(strsplit(ag$x[[i]], split=";")))    
+    ag$x[i] = median(numeric.vector, na.rm=T)
+    ag$new[i] = list(numeric.vector)
+  }
+  colnames(ag) = c("timestamp", "Elance", "lista")
+  ag$Elance = unlist(ag$Elance)
+  ag$lista = gsub("[c(]", "", as.character(ag$lista))
+  ag$lista = gsub(")", "", ag$lista) 
+  ag$lista = gsub(",", ";", ag$lista)
+  ag$lista = gsub("\n", "", ag$lista)
+  ag$lista = gsub(" ", "", ag$lista)
+  nome.file = paste(output.dir, gsub(source.dir, "", f), sep="/")
+  write.csv(ag, file=nome.file, quote=F, row.names=F, sep=",")
+  rm(ag)
+}
+
+iterate.files = function(files) {
+  for(f in files) {
+    generating.csv.file(f)
+  }  
+}
+
+iterate.files(files)
